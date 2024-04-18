2b738501c30fb753f5aba31c546bfc135089a93d
0ut0fm1nd <qinm2023@gmail.com>
2024-04-18 16:59:52 +0800

    code optimization

--- src/RequestEventStream.ts
+++ src/RequestEventStream.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
-import { workspace } from "vscode";
 import { FetchStream } from "./FetchStream";
+import { MAX_TOKENS_CHAT, MODEL_ENV, SERVER_ADDR_CHAT, STOP_WORDS } from "./consts";
+
 
 let abortController = new AbortController();
 
@@ -9,35 +10,31 @@ export async function stopEventStream() {
 }
 
 export async function postEventStream(prompt: string, msgCallback: (data: string) => any, doneCallback: () => void, errorCallback: (err: any) => void) {
-    const serverAddress = workspace.getConfiguration("CodeShell").get("ServerAddress") as string;
-    const maxtokens = workspace.getConfiguration("CodeShell").get("ChatMaxTokens") as number;
-
-    const modelEnv = workspace.getConfiguration("CodeShell").get("RunEnvForLLMs") as string;
     var uri = "";
     var body = {};
-    if ("CPU with llama.cpp" == modelEnv) {
+    if ("CPU with llama.cpp" == MODEL_ENV) {
         uri = "/completion"
         body = {
-            "prompt": "|<end>|" + prompt, "n_predict": maxtokens, 
-            "temperature": 0.8, "repetition_penalty": 1.2, "top_k":40,  "top_p":0.95, "stream": true, 
-            "stop": ["|<end>|", "|end|", "<|endoftext|>", "## human"]
+            "prompt": "|<end>|" + prompt, "n_predict": MAX_TOKENS_CHAT,
+            "temperature": 0.8, "repetition_penalty": 1.2, "top_k": 40, "top_p": 0.95, "stream": true,
+            "stop": STOP_WORDS
         };
     }
-    if ("GPU with TGI toolkit" == modelEnv) {
+    if ("GPU with TGI toolkit" == MODEL_ENV) {
         uri = "/generate_stream"
         // uri = "/codeshell-code/assistants"
         body = {
             "inputs": prompt,
             "parameters": {
-                "max_new_tokens": maxtokens,
-                "temperature": 0.6, "repetition_penalty": 1.2, "top_p": 0.95, "do_sample": true, 
-                "stop": ["|<end>|", "|end|", "<|endoftext|>", "## human"]
+                "max_new_tokens": MAX_TOKENS_CHAT,
+                "temperature": 0.6, "repetition_penalty": 1.2, "top_p": 0.95, "do_sample": true,
+                "stop": STOP_WORDS
             }
         };
     }
     abortController = new AbortController();
     new FetchStream({
-        url: serverAddress + uri,
+        url: SERVER_ADDR_CHAT + uri,
         requestInit: {
             method: "POST",
             headers: {
---
+++ src/consts.ts
@@ -0,0 +1,16 @@
+import { extensions, workspace } from "vscode";
+
+export const EXTENSION_ID = "WisdomShell.codeshell-vscode";
+export const EXTENSION_PATH = extensions.getExtension(EXTENSION_ID)?.extensionPath;
+
+
+export const END_OF_TEXT = "<|endoftext|>";
+export const STOP_WORDS = [END_OF_TEXT, "|<end>|", "|end|", "## human"];
+
+
+export const CODESHELL_CONFIG = workspace.getConfiguration("CodeShell");
+export const SERVER_ADDR_CHAT = CODESHELL_CONFIG.get("ServerAddress");
+export const SERVER_COMPLETION = CODESHELL_CONFIG.get("ServerAddress");
+export const MAX_TOKENS_CHAT =  CODESHELL_CONFIG.get("ChatMaxTokens");
+export const MAX_TOKENS_COMPLETION = CODESHELL_CONFIG.get("CompletionMaxTokens");
+export const MODEL_ENV = CODESHELL_CONFIG.get("RunEnvForLLMs");
