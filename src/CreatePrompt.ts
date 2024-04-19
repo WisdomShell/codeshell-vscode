7eb18f67d6ed3d2b7211462686f342d0f2077da6
0ut0fm1nd <qinm2023@gmail.com>
2024-04-19 09:00:43 +0800

    l10n code optimization

--- src/CreatePrompt.ts
+++ src/CreatePrompt.ts
@@ -1,29 +1,31 @@
-import { translate } from "./LanguageHelper";
-
+import { l10n } from "vscode";
 export function createPromptCodeExplain(language: string, functionCode: string): string {
-	return `${translate("explain")} ${language} ${translate("code")}：\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Please explain the following {0} code", language)} \n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptCodeImprove(language: string, functionCode: string): string {
-	return `${translate("optimize_rewrite")} ${language} ${translate("code")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Please optimize and rewrite the following {0} code", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptCodeClean(language: string, functionCode: string): string {
-	return `${translate("clean_up")} ${language} ${translate("code")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Please clean and rewrite the following {0} code", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptGenerateComment(language: string, functionCode: string): string {
-	return `${translate("following")} ${language} ${translate("comment")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Please generate comments for the following {0} code", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptGenerateUnitTest(language: string, functionCode: string): string {
-	return `${translate("following")} ${language} ${translate("unit_test")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Please generate unit tests for the following {0} code", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptCheckPerformance(language: string, functionCode: string): string {
-	return `${translate("check")} ${language} ${translate("code")}${translate("performance_issue")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Check the following {0} code for any performance issues, and please provide optimization suggestions", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
 
 export function createPromptCheckSecurity(language: string, functionCode: string): string {
-	return `${translate("check")} ${language} ${translate("code")}${translate("security_issue")}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+	return `${l10n.t("Check the following {0} code for any security issues, and please provide optimization suggestions", language)}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
+}
+export function createPromptAskingQuestion(language: string, functionCode: string, inputText: string,): string {
+	return `${inputText}\n\`\`\`${language}\n${functionCode}\n\`\`\`\n`;
 }
--- src/RequestCompletion.ts
+++ src/RequestCompletion.ts
@@ -1,25 +1,21 @@
 /* eslint-disable */
 
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { workspace } from "vscode";
-import { translate } from "./LanguageHelper";
+import { l10n } from "vscode";
+import { END_OF_TEXT, MAX_TOKENS_COMPLETION, MODEL_ENV, SERVER_COMPLETION, STOP_WORDS } from "./consts";
 export interface CompletionResponse {
     "generated_text"?: string;
 }
 
 export async function postCompletion(fimPrefixCode: string, fimSuffixCode: string): Promise<string | undefined> {
-    const serverAddress = workspace.getConfiguration("CodeShell").get("ServerAddress") as string;
-    let maxtokens = workspace.getConfiguration("CodeShell").get("CompletionMaxTokens") as number;
-
-    const modelEnv = workspace.getConfiguration("CodeShell").get("RunEnvForLLMs") as string;
-    if ("CPU with llama.cpp" == modelEnv) {
+    if ("CPU with llama.cpp" == MODEL_ENV) {
         let data = {
             "input_prefix": fimPrefixCode, "input_suffix": fimSuffixCode,
-            "n_predict": maxtokens, "temperature": 0.2, "repetition_penalty": 1.0, "top_k": 10, "top_p": 0.95,
-            "stop": ["|<end>|", "|end|", "<|endoftext|>", "## human"]
+            "n_predict": MAX_TOKENS_COMPLETION, "temperature": 0.2, "repetition_penalty": 1.0, "top_k": 10, "top_p": 0.95,
+            "stop": STOP_WORDS
         };
         console.debug("request.data:", data)
-        const response = await axiosInstance.post(serverAddress + "/infill", data);
+        const response = await axiosInstance.post(SERVER_COMPLETION + "/infill", data);
         var content = "";
         const respData = response.data as string;
         const dataList = respData.split("\n\n");
@@ -29,29 +25,29 @@ export async function postCompletion(fimPrefixCode: string, fimSuffixCode: strin
             }
         }
         console.debug("response.data:", content)
-        return content.replace("<|endoftext|>", "");
+        return content.replace(END_OF_TEXT, "");
     }
-    if ("GPU with TGI toolkit" == modelEnv) {
+    if ("GPU with TGI toolkit" == MODEL_ENV) {
         const prompt = `<fim_prefix>${fimPrefixCode}<fim_suffix>${fimSuffixCode}<fim_middle>`;
         let data = {
             "inputs": prompt,
             "parameters": {
-                "max_new_tokens": maxtokens, "temperature": 0.2, "repetition_penalty": 1.2, "top_p": 0.99, "do_sample": true,
-                "stop": ["|<end>|", "|end|", "<|endoftext|>", "## human"]
+                "max_new_tokens": MAX_TOKENS_COMPLETION, "temperature": 0.2, "repetition_penalty": 1.2, "top_p": 0.99, "do_sample": true,
+                "stop": STOP_WORDS
             }
         };
         console.debug("request.data:", data)
         const uri = "/generate"
         // const uri = "/codeshell-code/completion"
-        const response = await axiosInstance.post<CompletionResponse>(serverAddress + uri, data);
+        const response = await axiosInstance.post<CompletionResponse>(SERVER_COMPLETION + uri, data);
         console.debug("response.data:", response.data)
-        return response.data.generated_text?.replace("<|endoftext|>", "");
+        return response.data.generated_text?.replace(END_OF_TEXT, "");
     }
 }
 
 const axiosInstance: AxiosInstance = axios.create({
     timeout: 60000,
-    timeoutErrorMessage: translate("timeout")
+    timeoutErrorMessage: l10n.t("Request timeout, please try again later")
 });
 
 axiosInstance.interceptors.request.use(
